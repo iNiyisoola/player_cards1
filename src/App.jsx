e3343b0933ebe518bf1bc9fccc9e48dc973c5763
iNiyisoola <niyisoolainioluwa@gmail.com>
2025-04-27 23:10:42 +0100

    add calculate overallrating

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import cr7 from './assets/cristiano.jpeg'
 
 function App() {
 
+    const messistats = {attack: "98%", speed: "89%", technique: "98%", dribbling: "92%", shooting: "95%", passing: "96%", defence: "49%"};
+    const cr7stats = {attack: "98%", speed: "90%", technique: "94%", dribbling: "91%", shooting: "95%", passing: "94%", defence: "49%"};
+
   return(
     <>
-      <Card playerFirstName="Lionel" playerLastName="Messi" playerImage ={leo} attack="98%" speed="89%" technique="98%" dribbling="92%" shooting="95%" passing="96%" defence="49%"/>
-      <Card playerFirstName="Cristiano" playerLastName="Ronaldo" playerImage ={cr7} attack="98%" speed="90%" technique="94%" dribbling="91%" shooting="95%" passing="94%" defence="49%"/>      
+      <Card playerFirstName="Lionel" playerLastName="Messi" playerImage ={leo} {...messistats}/>
+      <Card playerFirstName="Cristiano" playerLastName="Ronaldo" playerImage ={cr7} {...cr7stats}/>      
 
       {/* <PlayerDetails/> */}
     </>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,41 +1,52 @@
-import leo from './assets/lionel-messi.jpeg'
-import cr7 from './assets/cristiano.jpeg'
 
 function Card(props){
+
+  const calculateOverall = () =>{
+    const stats = [
+                  props.attack, 
+                  props.speed, 
+                  props.technique, 
+                  props.shooting, 
+                  props.passing, 
+                  props.dribbling, 
+                  props.defence
+                  ];
+
+    const numbers = stats.map(stat => parseInt(stat.replace('%', '')));
+    const sum = numbers.reduce((a, b) => a + b, 0);
+    const average = Math.round(sum / numbers.length);
+    return `${average}%`;
+  }
+
+  const overall = calculateOverall();
+
+
   return(
     <>
       <div className="player-card">
-          <img className="player-image" src={props.playerImage} alt=""></img>
-          <div className="player-name">
-            <span className="player-first-name">{props.playerFirstName}</span> 
-            <br></br>
-            <span className="player-last-name">{props.playerLastName}</span>
+        <div className="image-stats-div">
+          {/* <span className="image-div"> */}
+            <img className="player-image" src={props.playerImage} alt=""></img>
+          {/* </span> */}
+          <div className="stats-div">
+            <p className="stats"><strong>ATT: </strong> {props.attack}</p>
+            <p className="stats"><strong>SPD: </strong> {props.speed}</p>
+            <p className="stats"><strong>TEC: </strong> {props.technique}</p>
+            <p className="stats"><strong>SHT: </strong> {props.shooting}</p>
+            <p className="stats"><strong>PAS: </strong> {props.passing}</p>
+            <p className="stats"><strong>DRB: </strong> {props.dribbling}</p>
+            <p className="stats"><strong>DEF: </strong> {props.defence}</p>
           </div>
-          <div className="player-stats">
-            <p><strong>ATT: </strong> {props.attack}</p>
-            <p><strong>SPD: </strong> {props.speed}</p>
-            <p><strong>SHT: </strong> {props.shooting}</p>
-            <p><strong>PAS: </strong> {props.passing}</p>
-            <p><strong>DEF: </strong> {props.defence}</p>
-            <p><strong>TECN: </strong> {props.technique}</p>
-            <p><strong>DRBL: </strong> {props.dribbling}</p>
+        </div>
+        <div className="player-name">
+          <span className="player-first-name">{props.playerFirstName}</span> 
+          <span className="player-last-name">{props.playerLastName}</span>
+        </div>
+        <span>{overall}</span>
 
-          </div>
       </div>
 
-      {/* <div className="player-card">
-          <img className="player-image" src={cr7} alt=""></img>
-          <h2 className="player-name">{props.playerName}</h2>
-          <ul className="cr7-stats">
-            <li><strong>ATT:</strong> {props.attack}</li>
-            <li><strong>SPD:</strong> {props.speed}</li>
-            <li><strong>TECN:</strong> {props.technique}</li>
-            <li><strong>DRBL:</strong> {props.dribbling}</li>
-            <li><strong>SHT:</strong> {props.shooting}</li>
-            <li><strong>PAS:</strong> {props.passing}</li>
-            <li><strong>DEF:</strong> {props.defence}</li>
-          </ul>
-      </div> */}
+      {}
     </>
   );
 }
